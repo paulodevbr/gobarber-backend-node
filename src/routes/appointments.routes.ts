78cc6b67c758835a5e8cda7a9bc3f1dbfa455a7c
Paulo de Oliveira Neto <paulo-gyn@outlook.com>
2020-07-22 17:56:34 -0300

    Create reference User -> Appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -15,7 +15,7 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  const { provider, date } = request.body;
+  const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
@@ -23,7 +23,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
   try {
     const appointment = await createAppointment.execute({
-      provider,
+      provider_id,
       date: parsedDate,
     });
     return response.json(appointment);
